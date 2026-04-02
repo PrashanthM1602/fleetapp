a4a9d7b809882f3722f27ef57252bb702c77220d
Prashanth <prashanthrikkie775@gmail.com>
2026-04-02 20:05:03 +0530

    fix render errors

--- fleet/settings.py
+++ fleet/settings.py
@@ -10,7 +10,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com']
 
@@ -159,7 +159,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get("GOOGLE_SECRET")
 # SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ['email', 'profile']
 
 # Redirect URL (must match what you added in Google Cloud)
-SOCIAL_AUTH_GOOGLE_OAUTH2_REDIRECT_URI = "https://your-app.onrender.com/auth/complete/google-oauth2/"
+SOCIAL_AUTH_GOOGLE_OAUTH2_REDIRECT_URI = "https://fleetapp-jym7.onrender.com/auth/complete/google-oauth2/"
 
 # Redirect after login/logout
 LOGIN_REDIRECT_URL = '/dashboard/'
@@ -242,8 +242,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
-AWS_STORAGE_BUCKET_NAME = 'fleetapp-media-storage-12345-792233689462-ap-southeast-2-an'
-AWS_S3_REGION_NAME = 'ap-southeast-2'
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
 
 STORAGES = {
     "default": {
