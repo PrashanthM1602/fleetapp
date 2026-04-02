17266f036075a2cdb7c94f88a8113c0d8dbf495e
Prashanth <prashanthrikkie775@gmail.com>
2026-04-02 19:30:41 +0530

    fix sqlite fallback

--- fleet/settings.py
+++ fleet/settings.py
@@ -89,11 +89,20 @@ WSGI_APPLICATION = 'fleet.wsgi.application'
 # }
 
 
+import os
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
-}
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
